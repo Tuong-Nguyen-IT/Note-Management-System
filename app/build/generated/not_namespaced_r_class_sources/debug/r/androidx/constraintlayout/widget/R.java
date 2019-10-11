6ad9d54552a066d740014ae0ab0aa92a02177215
Hoang My <17050055@student.bdu.edu.vn>
2019-10-11 22:10:40 +0700

    design form login

--- app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
@@ -74,28 +74,19 @@ public final class R {
         private id() {}
 
         public static final int bottom = 0x7f080024;
-        public static final int end = 0x7f08004f;
-        public static final int gone = 0x7f080060;
-        public static final int invisible = 0x7f08006b;
-        public static final int left = 0x7f080070;
-        public static final int packed = 0x7f080093;
-        public static final int parent = 0x7f080095;
-        public static final int percent = 0x7f080099;
-<<<<<<< HEAD
-        public static final int right = 0x7f08009f;
-        public static final int spread = 0x7f0800c5;
-        public static final int spread_inside = 0x7f0800c6;
-        public static final int start = 0x7f0800cb;
-        public static final int top = 0x7f0800e8;
-        public static final int wrap = 0x7f0800fe;
-=======
-        public static final int right = 0x7f0800a0;
-        public static final int spread = 0x7f0800c6;
-        public static final int spread_inside = 0x7f0800c7;
-        public static final int start = 0x7f0800cc;
-        public static final int top = 0x7f0800e8;
-        public static final int wrap = 0x7f0800f8;
->>>>>>> d1379d168fa7088ac9bde57d5684733496856462
+        public static final int end = 0x7f080054;
+        public static final int gone = 0x7f080066;
+        public static final int invisible = 0x7f080071;
+        public static final int left = 0x7f080076;
+        public static final int packed = 0x7f080099;
+        public static final int parent = 0x7f08009b;
+        public static final int percent = 0x7f08009f;
+        public static final int right = 0x7f0800a6;
+        public static final int spread = 0x7f0800cc;
+        public static final int spread_inside = 0x7f0800cd;
+        public static final int start = 0x7f0800d2;
+        public static final int top = 0x7f0800ee;
+        public static final int wrap = 0x7f080105;
     }
     public static final class styleable {
         private styleable() {}
--- app/src/main/java/com/example/notemanagementsystem/LoginActivity.java
+++ app/src/main/java/com/example/notemanagementsystem/LoginActivity.java
@@ -2,13 +2,61 @@ package com.example.notemanagementsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class LoginActivity extends AppCompatActivity {
+    private TextView txtEmail;
+    private TextView txtPassword;
+    private CheckBox checkRemember;
+    private Button btnSignIn;
+    private Button btnExit;
+    private FloatingActionButton fabAddAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        addControls();
+        addEvents();
+    }
+
+    private void addEvents() {
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // xu ly dang nhap
+            }
+        });
+        btnExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.exit(1);
+            }
+        });
+        fabAddAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private void addControls() {
+        txtEmail = findViewById(R.id.txtEmail);
+        txtPassword = findViewById(R.id.txtPassword);
+        checkRemember = findViewById(R.id.checkRemember);
+        btnSignIn = findViewById(R.id.btnSignIn);
+        btnExit = findViewById(R.id.btnExit);
+        fabAddAccount = findViewById(R.id.fabAddAccount);
+
     }
 }
